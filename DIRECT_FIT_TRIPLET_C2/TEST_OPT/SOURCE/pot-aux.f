1ff8974bd33715ea464bcb28d1401f0c8b0cf9a4
Cmurilochem <114645116+Cmurilochem@users.noreply.github.com>
2022-09-29 09:55:35 +0200

    Delete DIRECT_FIT_TRIPLET_C2 directory

--- DIRECT_FIT_TRIPLET_C2/TEST_OPT/SOURCE/pot-aux.f
+++
@@ -1,9 +0,0 @@
-C234567
-      function POTEN(R,NRD)
-      implicit real*8(A-H,O-Z)
-      CALL CHIPR_DIAT(R,POT,.FALSE.,DVDR)
-      POTEN=POT
-      RETURN
-      END
-
-c  !!!****************************************************************!!!
